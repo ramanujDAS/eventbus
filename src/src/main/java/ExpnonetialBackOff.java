448e641a9cfbc0f8fb2506bb6bac5da04db373bd
ramanujsingh <ramanuj1st@gmail.com>
2024-12-27 01:30:51 +0530

    refactoring pending for Common Logic for retry

--- src/src/main/java/ExpnonetialBackOff.java
+++ src/src/main/java/ExpnonetialBackOff.java
@@ -2,37 +2,15 @@ import java.util.function.Function;
 
 public class ExpnonetialBackOff<P , R> extends RetryAlgorithm<P , R> {
 
-    private final int maxAttempts;
 
     public ExpnonetialBackOff(int maxAttempts) {
-        this.maxAttempts = maxAttempts;
+        super(maxAttempts, -1);
     }
+
+
     @Override
-    public void retry(Function<P, R> task , P parameter , int attempt) {
-
-        try{
-           task.apply(parameter);
-        }catch (Exception e){
-            if(e.getCause() instanceof RetryableException){
-               if(attempt < maxAttempts){
-                   attempt++;
-                   long timeToWait = (long) (Math.pow(2, attempt - 1 ) * 1000);
-                   try {
-                       Thread.sleep(timeToWait);
-                   } catch (InterruptedException ex) {
-                       throw new RuntimeException(ex);
-                   }
-                   retry(task,parameter , attempt + 1 );
-               }else{
-                   throw new RetryLimitExceedException("Retry limit exceeded");
-               }
-            }
-            else {
-                throw new RuntimeException(e);
-            }
-            System.out.println("got some error");
-        }
-
+    long getWaitTime() {
+        return (long) Math.pow(2, maxAttempts - 1 ) * 1000;
     }
 }
 
--- src/src/main/java/RetryAlgorithm.java
+++ src/src/main/java/RetryAlgorithm.java
@@ -1,6 +1,39 @@
 import java.util.function.Function;
 
 public abstract class RetryAlgorithm<P , R> {
+    final int maxAttempts;
+    final int intervalTime;
 
-    public abstract void retry(Function<P , R> task, P parameter , int attempt);
+    protected RetryAlgorithm(int maxAttempts, int intervalTime) {
+        this.maxAttempts = maxAttempts;
+        this.intervalTime = intervalTime;
+    }
+
+    public  void retry(Function<P , R> task, P parameter , int attempt){
+        try {
+            task.apply(parameter);
+        } catch (Exception e) {
+            if (e.getCause() instanceof RetryableException) {
+                if (attempt < maxAttempts) {
+                    attempt++;
+                    long timeToWait = getWaitTime();
+                    try {
+                        Thread.sleep(timeToWait);
+                    } catch (InterruptedException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    retry(task, parameter, attempt + 1);
+                } else {
+                    throw new RetryLimitExceedException("Retry limit exceeded");
+                }
+            } else {
+                throw new RuntimeException(e);
+            }
+            System.out.println("got some error");
+        }
+
+    }
+
+     abstract long getWaitTime();
+
 }
